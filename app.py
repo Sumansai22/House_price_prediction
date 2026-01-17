ad4a2b55fae1a20e3b7d4f46cae9b9fd29d24c18
B.SUMANTH <vtu29228@veltech.edu.in>
2026-01-17 16:45:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import pickle as pk
+import streamlit as st
+
+
+
+model = pk.load(open(r"C:\Users\Sumanth\OneDrive\Desktop\house prediction model\House_prediction_model.pk1", "rb"))
+
+input = pd.DataFrame([['Electronic City Phase II',1000.0,3.0,2.0,3]],columns=['location','total_sqft','bath','balcony','bedrooms'])
+st.header('house price prediction')
+data = pd.read_csv('C:/Users/Sumanth/OneDrive/Desktop/house prediction model/cleaned_data.csv')
+
+loc = st.selectbox('Choose the location', data['location'].unique())
+sqft = st.number_input('Enter Total sqft')
+beds = st.number_input('Enter No of Bedrooms')
+bath = st.number_input('Enter No of Bathrooms')
+balcony = st.number_input('Enter No of balconies')
+
+input = pd.DataFrame([[loc,sqft,bath,balcony,beds]])
+
+if st.button("predict price"):
+    output = model.predict(input)
+    out_str = 'price of the House is '+ str(output[0]*100000)
